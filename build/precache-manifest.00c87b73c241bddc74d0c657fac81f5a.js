a5d18cf0c7d37a0d7d4358da39118ad2ce8a429b
MaartenGr <maarten_grootendorst@hotmail.com>
2019-07-22 20:35:06 +0200

    test

---
+++ build/precache-manifest.00c87b73c241bddc74d0c657fac81f5a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5b62a65046180fbca57fc4d94c193a1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1b7974b9ce30b513f696",
+    "url": "/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "3a8cc4d7bf37e66324e4",
+    "url": "/static/js/2.d68f1bfa.chunk.js"
+  },
+  {
+    "revision": "1b7974b9ce30b513f696",
+    "url": "/static/js/main.3cbb138f.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
